6e603dd48e53559386d6c241d4052e091f4a3d2d
TIAGO JESUS PEREIRA <03313490000@senacrs.edu.br>
2025-09-25 20:20:43 -0300

    feat/refactor: Create admin login route and corretion of the admn file tree

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,30 @@ import Login from './Login.tsx'
 import Detalhes from './Detalhes.tsx'
 import MinhasReservas from './MinhasReservas.tsx'
 
+// Rotas do Administrador
+//import AdminLayout from './admin/AdminLayout.tsx'
+import AdminLogin from './admin/AdminLogin.tsx'   
+//import AdminDashboard from './admin/AdminDashboard.tsx'
+
 
 import Layout from './Layout.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const rotas = createBrowserRouter([
+  {
+    path: "/admin/login",
+    element: <AdminLogin />,   // rota do form de login sem o Layout da Área Administrativa
+  },
+  {
+    path: "/admin",
+    //element: <AdminLayout />,  // layout principal do admin com menus e outlet
+    children: [
+      //{ index: true, element: <AdminDashboard /> },          // rota /admin
+      // { path: "carros", element: <AdminCarros /> },          // rota /admin/carros
+      // { path: "carros/novo", element: <AdminNovoCarro /> },  // ...
+      // { path: "propostas", element: <AdminPropostas /> },  // ...
+    ],
+  },
   {
     path: '/',
     element: <Layout />,
