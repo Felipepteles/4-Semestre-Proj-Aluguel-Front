3d672bdf3211a5ba0a7fe1078b6ce1380e33b96e
Tiago Pereira <sd85infantaria@hotmail.com>
2025-10-15 11:23:56 -0300

    feat: include instructions of reserva in Detalhes.tsx

--- src/Detalhes.tsx
+++ src/Detalhes.tsx
@@ -1,5 +1,5 @@
 import type { FerramentaType } from "./utils/FerramentaType"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import { useClienteStore } from "./context/ClienteContext"
 import { toast } from 'sonner'
@@ -19,7 +19,7 @@ export default function Detalhes() {
   const [dataInicio, setDataInicio] = useState<Date | null>(new Date())
   const [dataFim, setDataFim] = useState<Date | null>(null)
   const [ferramenta, setFerramenta] = useState<FerramentaType>()
-
+  const navigate = useNavigate()
   const { cliente } = useClienteStore()
 
   const { register, handleSubmit, reset } = useForm<Inputs>()
@@ -58,24 +58,23 @@ export default function Detalhes() {
     }
 
     if (response.status == 201) {
-      toast.success("Obrigado. Sua proposta foi enviada. Aguarde retorno")
+      toast.success("Obrigado. Sua solicitação de reserva foi enviada. Aguarde retorno")
       reset()
+      navigate("/")
     } else {
-      toast.error("Erro...1 Não foi possível enviar sua proposta")
+      toast.error("Erro...1 Não foi possível enviar sua solicitação de reserva")
     }
   }
 
   return (
     <>
-      <section className="flex mt-6 mx-auto flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row md:max-w-5xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-lg shadow-gray-500/70">
+      <section className="flex mt-6 mx-auto flex-col items-center bg-white border border-gray-200 rounded-lg md:flex-row md:max-w-7xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 shadow-lg shadow-gray-500/70">
         <img className="object-cover w-full rounded-t-lg h-96 md:h-2/4 md:w-2/4 md:rounded-none md:rounded-s-lg"
           src={ferramenta?.foto} alt="Foto da Ferramenta" />
         <div className="flex flex-col justify-between p-4 leading-normal">
           <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {ferramenta?.marca.nome}
-          </h5>
-          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            {ferramenta?.nome}
+            <p>{ferramenta?.marca.nome} - {ferramenta?.nome}
+            </p>
           </h5>
           <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
             Descrição: {ferramenta?.descricao}
@@ -91,6 +90,19 @@ export default function Detalhes() {
             <>
               <h3 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white">
                 🙂Você pode fazer uma Reserva para esta ferramenta!</h3>
+              <h4>Instruções:</h4>
+              <p>
+                1. Insira a data de início, que será a data que você vai retirar a ferramenta em nossa loja;
+              </p>
+              <p>
+                2. Insira a data final, que será a data que você vai fazer a devolução da ferramenta;
+              </p>
+              <p>
+                3. Confira o valor e clique em "Solicitar Reserva"; e
+              </p>
+              <p>
+                4. Pronto! Agora é só aguardar a confirmação da sua reserva que irá chegar na sua caixa de E-Mails ou conferir aqui mesmo, em "Minhas Reservas".
+              </p>
               <form onSubmit={handleSubmit(reservar)}>
                 <input type="text" className="mb-2 mt-4 bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-400 dark:focus:ring-blue-500 dark:focus:border-blue-500" value={`${cliente.nome} (${cliente.email})`} disabled readOnly />
                 <textarea id="message" className="mb-2 block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -118,7 +130,7 @@ export default function Detalhes() {
                     minDate={dataInicio!}
                   />
                 </div>
-                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reservar Ferramenta</button>
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 cursor-pointer">Solicitar Reserva</button>
               </form>
 
             </>
--- src/components/Titulo.tsx
+++ src/components/Titulo.tsx
@@ -17,61 +17,63 @@ export default function Titulo() {
     if (localStorage.getItem("clienteKey")) {
       localStorage.removeItem("clienteKey")
     }
-    navigate("/login")
+    navigate("/")
   }
   return (
-    <nav className="border-blue-800 bg-blue-700 dark:bg-blue-700 dark:border-blue-800 shadow-lg shadow-gray-500/70">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <img src="/logo.png" className="h-12" alt="Logo Caixa de Ferramentas" />
-          <span className="text-shadow-lg text-shadow-gray-800/30 self-center font-noto text-4xl font-bold whitespace-nowrap dark:text-white text-white text-shadow-inner">
-            Caixa de Ferramentas
-          </span>
-        </Link>
-        <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
-          <span className="sr-only">Abrir Menu</span>
-          <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-          </svg>
-        </button>
+    <>
+      <nav className="border-blue-800 bg-blue-700 dark:bg-blue-700 dark:border-blue-800 shadow-lg shadow-gray-500/70">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src="/logo.png" className="h-12" alt="Logo Caixa de Ferramentas" />
+            <span className="text-shadow-lg text-shadow-gray-800/30 self-center font-noto text-4xl font-bold whitespace-nowrap dark:text-white text-white text-shadow-inner">
+              Caixa de Ferramentas
+            </span>
+          </Link>
+          <button data-collapse-toggle="navbar-solid-bg" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-solid-bg" aria-expanded="false">
+            <span className="sr-only">Abrir Menu</span>
+            <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+          </button>
 
-        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
-          <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
-            <li className="flex items-center gap-4">
-              {cliente.id ?
-                <>
-                  <span className="block text-sm text-white">{cliente.nome}</span>
-                  <Dropdown
-                    label={<Avatar alt="User settings" img="/user.svg" className="cursor-pointer" rounded />}
-                    arrowIcon={false}
-                    inline
-                  >
-                    <DropdownHeader>
-                      <span className="block truncate text-sm font-medium">{cliente.email}</span>
-                    </DropdownHeader>
-                    <DropdownItem>
-                      <Link to="/minhasReservas">
-                        Minhas Reservas
-                      </Link>
-                    </DropdownItem>
-                    <DropdownItem>Settings</DropdownItem>
-                    <DropdownItem>Earnings</DropdownItem>
-                    <DropdownDivider />
-                    <DropdownItem>
-                      <span className="cursor-pointer" onClick={() => setConfirmModal(true)}>Sair</span>
-                    </DropdownItem>
-                  </Dropdown>
-                </>
-                :
-                <Link to="/login" className="block py-2 px-3 md:p-0 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:hover:text-shadow-md md:hover:text-shadow-white/30 md:hover:font-bold  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
-                  Identifique-se
-                </Link>
-              }
-            </li>
-          </ul>
+          <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+            <ul className="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
+              <li className="flex items-center gap-4">
+                {cliente.id ?
+                  <>
+                    <span className="block text-sm text-white">{cliente.nome}</span>
+                    <Dropdown
+                      label={<Avatar alt="User settings" img="/user.svg" className="cursor-pointer" rounded />}
+                      arrowIcon={false}
+                      inline
+                    >
+                      <DropdownHeader>
+                        <span className="block truncate text-sm font-medium">{cliente.email}</span>
+                      </DropdownHeader>
+                      <DropdownItem>
+                        <Link to="/minhasReservas">
+                          Minhas Reservas
+                        </Link>
+                      </DropdownItem>
+                      <DropdownItem>Settings</DropdownItem>
+                      <DropdownItem>Earnings</DropdownItem>
+                      <DropdownDivider />
+                      <DropdownItem>
+                        <span className="cursor-pointer" onClick={() => setConfirmModal(true)}>Sair</span>
+                      </DropdownItem>
+                    </Dropdown>
+                    <ConfirmModal title="Tem certeza que deseja sair do Sistema?" show={confirmModal} onClose={() => setConfirmModal(false)} onSuccess={clienteSair} />
+                  </>
+                  :
+                  <Link to="/login" className="block py-2 px-3 md:p-0 text-white rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-gray-700 md:hover:text-shadow-md md:hover:text-shadow-white/30 md:hover:font-bold  dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">
+                    Identifique-se
+                  </Link>
+                }
+              </li>
+            </ul>
+          </div>
         </div>
-        <ConfirmModal title="Tem certeza que deseja sair do Sistema?" show={confirmModal} onClose={() => setConfirmModal(false)} onSuccess={clienteSair} />
-      </div>
-    </nav>
+      </nav>
+    </>
   )
 }
