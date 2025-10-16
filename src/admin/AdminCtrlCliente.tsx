3e56d7810a822f9330d0968ed73dfb90983bad09
Tiago Jesus Pereira <sd85infantaria@hotmail.com>
2025-10-16 15:23:58 -0300

    fix: ajustment in to table header of cliente

--- src/admin/AdminCtrlCliente.tsx
+++ src/admin/AdminCtrlCliente.tsx
@@ -33,7 +33,7 @@ export default function AdminCtrlCliente() {
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
-                Nome do Cliente
+                Nome
               </th>
               <th scope="col" className="px-6 py-3">
                 E-mail
