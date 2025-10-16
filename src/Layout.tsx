da8ccdbd9f36321a685f20eb000f000ea6b80d80
Tiago Jesus Pereira <sd85infantaria@hotmail.com>
2025-10-16 17:41:37 -0300

    feat: alteration public Outlet bg

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -6,14 +6,10 @@ import { Rodape } from './components/Footer.tsx'
 export default function Layout() {
   return (
     <>
-      {/* <Titulo />
-      <Outlet />
-      <Toaster richColors position="top-center" />
-      <Rodape /> */}
       <div className="flex flex-col min-h-screen">
         <Titulo />
         
-        <main className="flex-grow bg-slate-50">
+        <main className="flex-grow bg-slate-400/50">
           <Outlet />
         </main>
 
