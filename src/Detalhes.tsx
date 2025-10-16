d75f492dfae8d92f9bca72720e12208943df1bc3
Tiago Jesus Pereira <sd85infantaria@hotmail.com>
2025-10-16 15:57:30 -0300

    fix: ajustment in to Datepicker width

--- src/Detalhes.tsx
+++ src/Detalhes.tsx
@@ -146,7 +146,7 @@ export default function Detalhes() {
                 </div>
               </div>
               <form onSubmit={handleSubmit(reservar)} className="space-y-4">
-                <div className="flex items-center mb-2">
+                <div className="flex items-center mb-2 max-w-full">
                   <Datepicker
                     title="Data de Início"
                     key="primeiro"
